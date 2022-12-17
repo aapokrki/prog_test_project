6cf9d68fe252ab4ff4d28590f34bf01f8b1b7ad5
Aapo Kärki <aapokr@gmail.com>
2022-12-18 00:01:15 +0200

    A few basic tests

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -1,17 +1,31 @@
-import { default as isEmpty } from "../src/isEmpty.js";
-import { expect } from 'chai';
+import { default as isEmpty } from "../src/isEmpty.js"
+import { expect } from "chai"
 
-describe('isEmpty.js. Checks if value is en empty object, collection, map or set', () => {
-    it('1. Testing with input', () => {
-        expect(isEmpty(null)).to.equal(true);
-        expect(isEmpty(true)).to.equal(true);
-        expect(isEmpty(1)).to.equal(true);
-        expect(isEmpty([1, 2, 3])).to.equal(false);
-        expect(isEmpty('abc')).to.equal(false);
-        expect(isEmpty({ 'a': 1 })).to.equal(false);
-
-    });
-    it('2. Testing with empty argument', () => {
-        expect(isEmpty()).to.equal(true);
-    });
-});
+describe("isEmpty.js. Checks if value is en empty object, collection, map or set", () => {
+  it("1. Testing with input", () => {
+    expect(isEmpty(null)).to.equal(true)
+    expect(isEmpty(true)).to.equal(true)
+    expect(isEmpty(1)).to.equal(true)
+    expect(isEmpty([1, 2, 3])).to.equal(false)
+    expect(isEmpty("abc")).to.equal(false)
+    expect(isEmpty({ "a": 1 })).to.equal(false)
+  })
+  it("2. Testing with empty argument", () => {
+    expect(isEmpty()).to.equal(true)
+  })
+  it("3. Testing with empty object", () => {
+    expect(isEmpty({})).to.equal(true)
+  })
+  it("4. Testing with empty array", () => {
+    expect(isEmpty([])).to.equal(true)
+  })
+  it("5. Testing with empty string", () => {
+    expect(isEmpty("")).to.equal(true)
+  })
+  it("6. Testing with empty map", () => {
+    expect(isEmpty(new Map())).to.equal(true)
+  })
+  it("7. Testing with empty set", () => {
+    expect(isEmpty(new Set())).to.equal(true)
+  })
+})
--- test/words.test.js
+++ test/words.test.js
@@ -1,17 +1,16 @@
-import { default as words } from "../src/words.js";
-import { expect } from 'chai';
+import { default as words } from "../src/words.js"
+import { assert, expect } from "chai"
 
-
-describe('words.js. Splits string into an array of its words', () => {
-    it('1. Testing with proper input', () => {
-        expect(words('fred, barney, & pebbles')).to.deep.equal(['fred', 'barney', 'pebbles']);
-        expect(words('fred, barney, & pebbles', /[^, ]+/g)).to.deep.equal(['fred', 'barney', '&', 'pebbles']);
-    });
-    it('2. Testing with empty string', () => {
-        expect(words('')).to.deep.equal([]);
-    });
-    it('3. Testing with empty argument', () => {
-        expect(words()).to.deep.equal([]);
-    });
-    
-});
+describe("words.js. Splits string into an array of its words", () => {
+  it("1. Testing with proper input", () => {
+    expect(words("fred, barney, & pebbles")).to.deep.equal(["fred", "barney", "pebbles"])
+    expect(words("fred, barney, & pebbles", /[^, ]+/g)).to.deep.equal(["fred", "barney", "&", "pebbles"])
+  })
+  it("2. Testing with empty string", () => {
+    expect(words("")).to.deep.equal([])
+  })
+  it("3. Testing with empty argument", () => {
+    console.log(typeof val)
+    assert.throw(() => words(), TypeError)
+  })
+})
